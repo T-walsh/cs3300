98132519bb2c932a18dada0400aeccb9f37e37db
Tanner <twalsh2@uccs.edu>
2021-11-04 18:31:25 +0000

    Rspec testing integration

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,6 +12,25 @@ RSpec.describe Project, type: :model do
       project = Project.new(title: "Title", description: "Some description content goes here")
       expect(project.save).to eq(true)
     end
+
+    it "ensures the description is present" do
+        project = Project.new(title: "Title")
+        expect(project.valid?).to eq(false)
+    end
+
+    context "scopes tests" do
+        let(:params) { { title: "Title", description: "some description" } }
+        before(:each) do
+          Project.create(params)
+          Project.create(params)
+          Project.create(params)
+        end
+    
+        it "should return all projects" do
+          expect(Project.count).to eq(3)
+        end
+    
+      end
   end
 
   context "scopes tests" do
