eb71d9d8c0da1f102d378b852a761e7339e19600
Tanner <twalsh2@uccs.edu>
2021-11-17 18:57:56 +0000

    All tests working before login imp

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+class User
+  def initialize(args = {}); end
+
+  def email
+    'someone@example.com'
+  end
+
+  def password
+    12021679918
+  end
+end
+
+describe User do
+  describe "#initialize" do
+    it "expects email and password param for user" do
+      u = User.new email: 'someone@example.com', password: 12021679918
+      expect(u.email).to eq('someone@example.com')
+      expect(u.password).to eq(12021679918)
+    end
+  end
 end
